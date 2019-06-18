aa313868df5d157a648ddd2dc76e2a02d961b2b1
wtwei <wutwei@qq.com>
2019-06-18 15:47:22 +0800

    feat: 增加获取坐席设备信息

---
+++ src/Api/CCC/Request/V20170705/ListAgentDevicesRequest.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace AliyunOpenApi\Api\CCC\Request\V20170705;
+
+use AliyunOpenApi\Core\RpcAcsRequest;
+
+class ListAgentDevicesRequest extends RpcAcsRequest
+{
+    function __construct()
+    {
+        parent::__construct("CCC", "2017-07-05", "ListAgentDevices", "ccc", "openAPI");
+        $this->setMethod("POST");
+    }
+
+    private $ramIds;
+
+    private $instanceId;
+
+    private $startTime;
+
+    private $stopTime;
+
+    public function getRamIds()
+    {
+        return $this->ramIds;
+    }
+
+    public function setRamIds($ramIds)
+    {
+        $this->ramIds                    = $ramIds;
+        $this->queryParameters["RamIds"] = $ramIds;
+    }
+
+    public function getInstanceId()
+    {
+        return $this->instanceId;
+    }
+
+    public function setInstanceId($instanceId)
+    {
+        $this->instanceId                    = $instanceId;
+        $this->queryParameters["InstanceId"] = $instanceId;
+    }
+
+    public function getStartTime()
+    {
+        return $this->startTime;
+    }
+
+    public function setStartTime($startTime)
+    {
+        $this->startTime                    = $startTime;
+        $this->queryParameters["StartTime"] = $startTime;
+    }
+
+    public function getStopTime()
+    {
+        return $this->stopTime;
+    }
+
+    public function setStopTime($stopTime)
+    {
+        $this->stopTime                    = $stopTime;
+        $this->queryParameters["StopTime"] = $stopTime;
+    }
+
+}
